6706d247d41093b10bb64c71d83b8563b399ad98
Matthew Barnes <taking1n1@gmail.com>
2019-09-24 16:39:22 +0100

    Updates

---
+++ precache-manifest.d231bc50e0f6145be9e88c6bb2b384a7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d8f21dc52e769e15f21514e98ba2f6",
+    "url": "/SameGameForReact/index.html"
+  },
+  {
+    "revision": "0fc09187f21c063f5c2a",
+    "url": "/SameGameForReact/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "1aa692c42979a4b4a3e8",
+    "url": "/SameGameForReact/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "0fc09187f21c063f5c2a",
+    "url": "/SameGameForReact/static/js/2.6417fd73.chunk.js"
+  },
+  {
+    "revision": "1aa692c42979a4b4a3e8",
+    "url": "/SameGameForReact/static/js/main.0df726c9.chunk.js"
+  },
+  {
+    "revision": "494236e70ff6fa9e4768",
+    "url": "/SameGameForReact/static/js/runtime-main.a4b93e15.js"
+  },
+  {
+    "revision": "b8bd58caa73045e8c3616c888bd3e89c",
+    "url": "/SameGameForReact/static/media/blank.fw.b8bd58ca.png"
+  },
+  {
+    "revision": "6c33a757715518458dccdab8959d7190",
+    "url": "/SameGameForReact/static/media/blue.fw.6c33a757.png"
+  },
+  {
+    "revision": "d2cdd8c87c45fcaa98c26555689bbb7c",
+    "url": "/SameGameForReact/static/media/cyan.fw.d2cdd8c8.png"
+  },
+  {
+    "revision": "c857b8efa27eb1d28f1e09ec02d4d2e9",
+    "url": "/SameGameForReact/static/media/green.fw.c857b8ef.png"
+  },
+  {
+    "revision": "5a79c0733a0847548a6be3f39c81f1cf",
+    "url": "/SameGameForReact/static/media/orange.fw.5a79c073.png"
+  },
+  {
+    "revision": "661f2f9648c2142b87f140d15b21545a",
+    "url": "/SameGameForReact/static/media/purple.fw.661f2f96.png"
+  },
+  {
+    "revision": "1ddc275df9562a646c996391fc4a7d82",
+    "url": "/SameGameForReact/static/media/red.fw.1ddc275d.png"
+  },
+  {
+    "revision": "57c30b6eaefec6e7d39194952370b9e1",
+    "url": "/SameGameForReact/static/media/yellow.fw.57c30b6e.png"
+  }
+]);
