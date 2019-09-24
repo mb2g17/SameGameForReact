4aaf47293892bdc751db58448a49e8600322f201
Matthew Barnes <taking1n1@gmail.com>
2019-09-24 04:23:12 +0100

    Now display bigger grid and can randomise

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.css';
+import _ from 'lodash';
+import styled from "styled-components";
 
 import { Board } from "./Board";
 import { Grid } from "../classes/Grid";
+
+import './App.css';
 
 interface Props {
     startingGrid?: Grid
@@ -12,16 +15,43 @@ interface State {
     grid: Grid
 }
 
+const CenteringDiv = styled.div`
+    width: "100vw";
+    display: flex;
+    justify-content: center;
+`;
+
 class App extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
+
+        // Sets up grid
         this.state = {
-            grid: this.props.startingGrid === undefined ? new Grid([
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]
-            ]) : this.props.startingGrid
+            grid: this.props.startingGrid !== undefined ? this.props.startingGrid : this.randomiseGrid(10)
         };
+    }
+
+    /**
+     * Randomises the grid
+     */
+    randomiseGrid(size: number): Grid {
+        // Creates a number grid
+        let numberGrid: number[][] = [];
+
+        for (let i = 0; i < size; i++)
+        {
+            numberGrid.push([]);
+            for (let j = 0; j < size; j++)
+            {
+                numberGrid[i].push(_.random(1,4));
+            }
+        }
+
+        let grid = new Grid(numberGrid);
+        this.setState({
+            grid: new Grid(numberGrid)
+        });
+        return grid;
     }
 
     render(): React.ReactElement<any> {
@@ -30,7 +60,11 @@ class App extends React.Component<Props, State> {
                 <h1>SameGame for React</h1>
                 <h2>by Matthew Barnes</h2>
 
-                <Board grid={this.state.grid} />
+                <CenteringDiv>
+                    <Board grid={this.state.grid} />
+                </CenteringDiv>
+
+                <button onClick={() => this.randomiseGrid(10)}>Randomise grid</button>
             </div>
         );
     }
